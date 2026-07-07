f04edc36e9056d8818b7554267180c2f2a276cea
Crazy10061 <feuchta@outlook.com>
2026-07-07 10:09:36 -0500

    Configure wallpaper and terminal alias in frog-init.sh
    Set the Modrinth wallpaper and add alias for fetch command.

--- archiso/airootfs/usr/local/bin/frog-init.sh
+++ archiso/airootfs/usr/local/bin/frog-init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Set up the Modrinth green background wallpaper
+
 xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorvirtual1/workspace0/last-image -s /etc/skel/Wallpapers/frog.png
 
-# Enable standard terminal settings
+
 echo "alias fetch='fastfetch'" >> ~/.bashrc
